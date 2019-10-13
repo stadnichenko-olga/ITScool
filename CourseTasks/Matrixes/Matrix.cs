0377fccea7e9db9d17349cb3a4e6814bf0ac512f
stadnichenko-olga <stadnichenko@gmail.com>
2019-10-13 22:56:03 +0500

    Matrixes. Версия 0.1

--- CourseTasks/Matrixes/Matrix.cs
+++ CourseTasks/Matrixes/Matrix.cs
@@ -9,35 +9,43 @@ namespace Matrixes
 
         public Matrix(int m, int n)
         {
-            if (n <= 0 || m<=0)
+            if (n <= 0 || m <= 0)
             {
-                throw new ArgumentException(String.Format("One or both dimensions is less than 1", n));
+                throw new ArgumentException("One or both dimensions is less than 1");
             }
             matrix = new Vector[m];
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < m; i++)
             {
-                matrix[i] = new Vector(m);
+                matrix[i] = new Vector(n);
             }
         }
 
         public Matrix(double[,] array)
         {
+            if (array.Length == 0)
+            {
+                throw new ArgumentException($"Dimensions of {nameof(array)} is less than 1");
+            }
             int m = array.GetUpperBound(0) + 1;
-            int n = array.GetUpperBound(2) + 1;
+            int n = array.GetUpperBound(1) + 1;
             matrix = new Vector[m];
-            for (int i=0; i< m; i++)
+            for (int i = 0; i < m; i++)
             {
                 double[] vector = new double[n];
-                for (int j=0; j<n; j++)
+                for (int j = 0; j < n; j++)
                 {
                     vector[j] = array[i, j];
                 }
                 matrix[i] = new Vector(vector);
-            }            
+            }
         }
 
         public Matrix(Vector[] array)
         {
+            if (array.Length == 0)
+            {
+                throw new ArgumentException($"Dimensions of {nameof(array)} is less than 1");
+            }
             matrix = new Vector[array.Length];
             int i = 0;
             foreach (Vector vector in array)
@@ -46,165 +54,141 @@ namespace Matrixes
                 i++;
             }
         }
-        
-        public int GetFirstDimensions()
+
+        public Matrix(Matrix matrixToCopy)
         {
-            int m = 0;
-            foreach (var vector in matrix)
+            matrix = new Vector[matrixToCopy.GetFirstDimension()];
+            int i = 0;
+
+            foreach (Vector line in matrixToCopy.matrix)
             {
-                m++;
+                matrix[i] = new Vector(line);
+                i++;
             }
-            return m;
         }
 
-        public int GetSecondDimensions()
+        public int GetFirstDimension()
+        {
+            return matrix.Length;
+        }
+
+        public int GetSecondDimension()
         {
             return matrix[0].GetSize();
         }
 
+        public Matrix MultiplyByScalar(double scalar)
+        {
+            Matrix result = new Matrix(GetFirstDimension(), GetSecondDimension());
+
+            for (int i = 0; i < GetFirstDimension(); i++)
+            {
+                result.matrix[i] = matrix[i].GetVectorMultiplicationByScalar(scalar);
+            }
+
+            return result;
+        }
+
+        public Vector MultiplyByVector(Vector vectorToMuliplicate)
+        {           
+            double[] result = new double[GetFirstDimension()];
+
+            for (int i = 0; i < GetFirstDimension(); i++)
+            {
+                result[i] = Vector.GetVectorsMultiplication(matrix[i], vectorToMuliplicate);
+            }
+
+            return new Vector(result);
+        }
+
+        public Matrix GetMatrixesSum(Matrix matrix2)
+        {
+            int m = Math.Max(GetFirstDimension(), matrix2.GetFirstDimension());
+            int n = Math.Max(GetSecondDimension(), matrix2.GetSecondDimension());
+            Matrix result = new Matrix(m, n);
+
+            int mMin = Math.Min(GetFirstDimension(), matrix2.GetFirstDimension());
+
+            for (int i = 0; i < mMin; i++)
+            {
+                result.matrix[i] = matrix[i].GetVectorsSum(matrix2.matrix[i]);
+            }
+
+            if (GetFirstDimension() != matrix2.GetFirstDimension())
+            {
+                for (int i = mMin; i < GetFirstDimension(); i++)
+                {
+                    Vector zero = new Vector(n);
+                    result.matrix[i] = matrix[i].GetVectorsSum(zero);
+                }
+
+                for (int i = mMin; i < matrix2.GetFirstDimension(); i++)
+                {
+                    Vector zero = new Vector(n);
+                    result.matrix[i] = matrix2.matrix[i].GetVectorsSum(zero);
+                }
+            }
+
+            return result;
+        }
+
+        public Matrix GetMatrixesDifference(Matrix matrix2)
+        {
+            return GetMatrixesSum(matrix2.MultiplyByScalar(-1));
+        }
+
+        public Vector GetVectorIndex(int i)
+        {
+            return new Vector(matrix[i]);
+        }
+
+        public Vector GetColumnIndex(int i)
+        {
+            double[] column = new double[GetFirstDimension()];
+            int j = 0;
+            foreach (Vector line in matrix)
+            {
+                column[j] = line.GetVectorCoordinate(i);
+            }
+            return new Vector(column);
+        }
+
+        public void SetVectorIndex(int i, Vector value)
+        {
+            matrix[i] = new Vector(value);
+        }
+        
+        public Matrix GetTranspanentMatrix()
+        {
+            Matrix result = new Matrix(GetSecondDimension(),GetFirstDimension());
+
+            for (int i=0; i< GetSecondDimension(); i++)
+            {
+                result.matrix[i] = GetColumnIndex(i);
+            }
+
+            return result;
+        }
+
         public override string ToString()
         {
-            return string.Join(", ", vector);
-        }
-
-        public void GetVectorsSumm(Vector vector2)
-        {
-            for (int i = 0; i < Math.Min(GetSize(), vector2.GetSize()); i++)
+            string result = "{ ";
+            foreach (Vector line in matrix)
             {
-                vector[i] += vector2.vector[i];
-            }
-            Array.Resize(ref vector, Math.Max(GetSize(), vector2.GetSize()));
-            for (int i = GetSize() - 1; i < vector2.GetSize(); i++)
-            {
-                vector[i] = vector2.vector[i];
-            }
-        }
-
-        public void GetVectorsDifference(Vector vector2)
-        {
-            for (int i = 0; i < Math.Min(GetSize(), vector2.GetSize()); i++)
-            {
-                vector[i] -= vector2.vector[i];
-            }
-        }
-
-        public void GetVectorMultiplicationByScalar(double scalar)
-        {
-            for (int i = 0; i < GetSize(); i++)
-            {
-                vector[i] *= scalar;
-            }
-        }
-
-        public void GetReverseVector()
-        {
-            GetVectorMultiplicationByScalar(-1);
-        }
-
-        public double GetLength()
-        {
-            double length = 0;
-            foreach (double coordinate in vector)
-            {
-                length += coordinate * coordinate;
-            }
-            return Math.Sqrt(length);
-        }
-
-        public double GetVectorCoordinate(int i)
-        {
-            return vector[i - 1];
-        }
-
-        public void SetVectorCoordinate(int i, double value)
-        {
-            vector[i - 1] = value;
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (obj == null || GetType() != obj.GetType())
-            {
-                return false;
+                result = string.Concat(result, "{", string.Join(", ", line), " }, ");
             }
 
-            Vector vector2 = obj as Vector;
-            if (this.GetSize() != vector2.GetSize())
-            {
-                return false;
-            }
-            int i = 0;
-            foreach (double coordinate in vector2.vector)
-            {
-                if (coordinate != vector[i])
-                {
-                    return false;
-                }
-                i++;
-            }
-            return true;
+            return result = string.Concat(result, " }");
         }
 
-        public override int GetHashCode()
-        {
-            return ToString().GetHashCode();
+        public static Matrix GetMatrixesSum(Matrix matrix1, Matrix matrix2)
+        {            
+            return matrix1.GetMatrixesSum(matrix2);
         }
 
-        public static Vector GetVectorsSumm(Vector vector1, Vector vector2)
+        public static Matrix GetMatrixesDifference(Matrix matrix1, Matrix matrix2)
         {
-            int n = Math.Max(vector1.GetSize(), vector2.GetSize());
-            double[] vectorsSumm = new double[n];
-            int i = 0;
-            if (vector1.GetSize() <= vector2.GetSize())
-            {
-                foreach (double coordinate in vector1.vector)
-                {
-                    vectorsSumm[i] = coordinate + vector2.vector[i];
-                    i++;
-                }
-                while (i < n)
-                {
-                    vectorsSumm[i] = vector2.vector[i];
-                    i++;
-                };
-            }
-            else
-            {
-                foreach (double coordinate in vector2.vector)
-                {
-                    vectorsSumm[i] = coordinate + vector1.vector[i];
-                    i++;
-                }
-                while (i < n)
-                {
-                    vectorsSumm[i] = vector1.vector[i];
-                    i++;
-                };
-            }
-
-            return new Vector(vectorsSumm);
-        }
-
-        public static Vector GetVectorsDifference(Vector vector1, Vector vector2)
-        {
-            Vector vectorTemp = new Vector(vector2);
-            vectorTemp.GetReverseVector();
-            return GetVectorsSumm(vector1, vectorTemp);
-        }
-
-        public static double GetVectorsMultiplication(Vector vector1, Vector vector2)
-        {
-            if (vector1.GetSize() != vector2.GetSize())
-            {
-                throw new ArgumentException(String.Format("Dimensions are not equal"));
-            }
-            double vectorsMultiplication = 0;
-            for (int i = 0; i < vector1.GetSize(); i++)
-            {
-                vectorsMultiplication += vector1.vector[i] * vector2.vector[i];
-            }
-
-            return vectorsMultiplication;
+            return matrix1.GetMatrixesDifference(matrix2);
         }
     }
 }
