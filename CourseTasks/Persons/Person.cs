2d9c899da0e6a983f9a4791dffd401d8e6408c94
stadnichenko-olga <stadnichenko@gmail.com>
2019-11-15 00:43:07 +0500

    Переименование проекта в Лямбда

--- CourseTasks/Persons/Person.cs
+++ CourseTasks/Persons/Person.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Globalization;
 
-namespace Persons
+namespace Lyambda
 {
     class Person
     {
--- CourseTasks/Persons/Persons.cs
+++ CourseTasks/Persons/Persons.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Persons
+namespace Lyambda
 {
     class Persons
     {
